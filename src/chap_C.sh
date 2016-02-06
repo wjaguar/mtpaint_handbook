f0ed91c0b5a010de2c5b2e42a9a3a2a79323d90b
Dmitry Groshev <wjaguar@users.sourceforge.net>
2016-02-07 00:54:35 +0300

    Version 3.44.02

---
+++ src/chap_C.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+cat << END_IT_ALL
+<HTML>
+
+<HEAD>
+<TITLE>The mtPaint Handbook - Appendix C - The NEWS file from mtPaint</TITLE>
+</HEAD>
+
+<BODY>
+
+<P>
+<H1>Appendix C - The NEWS file from mtPaint</H1>
+
+<HR>
+
+`htmlify NEWS`
+<P>
+<HR>
+
+</BODY>
+</HTML>
+END_IT_ALL
---
+++ src/chap_D.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+cat << END_IT_ALL
+<HTML>
+
+<HEAD>
+<TITLE>The mtPaint Handbook - Appendix D - The README file from mtPaint</TITLE>
+</HEAD>
+
+<BODY>
+
+<P>
+<H1>Appendix D - The README file from mtPaint</H1>
+
+<HR>
+
+`htmlify README`
+<P>
+<HR>
+
+</BODY>
+</HTML>
+END_IT_ALL
